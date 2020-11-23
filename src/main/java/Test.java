1f1e6f4d57f320f10998c74afa681ef826d78490
Eoghan-Monahan <e.monahan1@nuigalway.ie>
2020-11-23 18:04:04 +0000

    Fix for console output

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,6 +6,7 @@ public class Test {
     public static void main (String args[]){
         ArrayList<Module> modules = new ArrayList<>();
         ArrayList<Course> courses = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
         Module testModule1 = new Module("Software Engineering","CT417");
         Module testModule2 = new Module("Machine Learning", "CT4101");
         modules.add(testModule1);
@@ -17,8 +18,11 @@ public class Test {
         courses.add(testCourse1);
         courses.add(testCourse2);
         Student tester = new Student("Test", 20, "8/11/2020", "12345678", modules, courses);
+        Student tester2 = new Student("Test2", 20, "8/11/2020", "12345678", modules, courses);
+        students.add(tester);
+        students.add(tester2);
         System.out.println("Student List");
-        System.out.println(tester);
+        System.out.println(students);
         testModule1.addStudent(tester);
         testCourse1.addModule(testModule1);
         System.out.println("Course List");
